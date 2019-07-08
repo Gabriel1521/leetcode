8590ee5edcbf3bf8bd530f1c054e7db6f7f5997b
Gabriel1521 <gabriela195487@gmail.com>
2019-07-08 15:12:47 +0800

    190706

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -29,3 +29,49 @@ class Solution:
 
 
         return list(result)
+
+
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+        res = set([])
+
+        for i in range(len(nums)):
+            l, r = i+1, len(nums)-1
+            while l<r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    path = (nums[i],nums[l],nums[r])
+                    res.add(path)
+                    l += 1
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return list(res)
+
+# 1. 两数之和
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = dict()
+        res = []
+        for i in range(len(nums)):
+            if nums[i] in d:
+                res = [d[nums[i]],i]
+            else:
+                r = target - nums[i]
+                d[r] = i
+        return res
--- Sorting Algorithm.py
+++ Sorting Algorithm.py
@@ -161,6 +161,29 @@ def heap_sort(arr):
        big_endian(arr, 0, end - 1)          # 重新调整子节点的顺序  从顶开始调整
    return arr
 
+def max_heap(arr, start, end):
+    root = start
+    while True:
+        child = root * 2 + 1
+        if child > end:
+            break
+        if child + 1 <= end and arr[child] < arr[child+1]:
+            child += 1
+        if arr[root] < arr[child]:
+            arr[root], arr[child] = arr[child], arr[root]
+            root = child
+        else:
+            break
+
+def heap_sort(arr):
+    first = len(arr)//2 - 1
+    for start in range(first,-1,-1):
+        max_heap(arr,start,len(arr)-1)
+    for end in range(len(arr)-1,0,-1):
+        arr[0], arr[end] = arr[end], arr[0]
+        max_heap(arr,0,end-1)
+    return arr
+
 
 def main():
    l = [3, 1, 4, 9, 6, 7, 5, 8, 2, 10]
