d2334a550de4ffd5c67ee61b8a148e4df38c4265
Gabriel1521 <gabriela195487@gmail.com>
2019-07-22 20:15:07 +0800

    190722

--- 94_Binary Tree Inorder Traversal.py
+++ 94_Binary Tree Inorder Traversal.py
@@ -29,6 +29,36 @@ class Solution(object):
         res.append(root.val)
         self.dfs(root.right,res)
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        # 用p当做指针
+        p = root
+        while p or stack:
+            # 把左子树压入栈中
+            while p:
+                stack.append(p)
+                p = p.left
+            # 输出 栈顶元素
+            tmp = stack.pop()
+            res.append(tmp.val)
+            # 看右子树
+            p = tmp.right
+        return res
+
+
 #144. 二叉树的前序遍历
 
 class Solution:
@@ -93,4 +123,3 @@ class Solution:
                     s.append((node.right,False))
                     s.append((node.left,False))
         return res
-        
