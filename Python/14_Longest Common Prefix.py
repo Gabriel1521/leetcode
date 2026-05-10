0352d6d1c2959facf38e983e333a3f22617f2c2c
Gabriel1521 <gabriela195487@gmail.com>
2026-05-11 06:28:32 +0800

    14 Longest Common Prefix

---
+++ Python/14_Longest Common Prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        for i,x in enumerate(zip(*strs)):
+            if len(set(x)) > 1:
+                return strs[0][:i]
+        
+        return min(strs)
